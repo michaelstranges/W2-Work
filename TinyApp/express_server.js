ce4daca468ea18cf9242b30d38686126b1ff092b
Michael Stranges <strangesm@gmail.com>
2018-03-25 14:39:14 +0000

    changed titles, fixed dead links

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -1,21 +1,18 @@
+//Constants
 const express = require("express");
 const app = express();
-//const cookieParser = require("cookie-parser")
 const cookieSession = require('cookie-session');
 const bcrypt = require("bcrypt");
-
 var PORT = process.env.PORT || 8080; // default port 8080
-
 app.set("view engine", "ejs");
-
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: "session",
   keys: ["topsecret"]
 }))
-//app.use(cookieParser())
 
+//User Database structure
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -29,6 +26,7 @@ const users = {
   }
 }
 
+//URL Database structure
 var urlDatabase = {
   test : {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -36,15 +34,17 @@ var urlDatabase = {
   }
 };
 
+//Basic Route Page
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+//Json File
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//uses templateVars
+// -/urls page using "urls_index" page to render
 app.get("/urls", (req, res) => {
   let theUser = req.session.user_id
   let templateVars = { urls : urlDatabase, user : users[req.session.user_id] };
@@ -55,11 +55,12 @@ app.get("/urls", (req, res) => {
   }
 })
 
+// -/hello page
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//uses templateVars
+//Page to add a new shortURL to the login users page
 app.get("/urls/new", (req, res) => {
   let templateVars = { user : users[req.session.user_id]  };
 // if cookies object is empty (equal 0) then go to login
@@ -70,32 +71,29 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-//uses templateVars
+//Link edit page
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase, user : users[req.session.user_id] };
   res.render("urls_show", templateVars);
 });
 
+//POST requests to /urls helps render main index page.  Send all the data seen below.
 app.post("/urls", (req, res) => {
-  //console.log(req.body);  // debug statement to see POST parameters
   let newID = req.session.user_id;
   let shortURLID = generateRandomString()
   let templateVars = { urls : urlDatabase, user : users[req.session.user_id] };
 
   urlDatabase[req.session.user_id][shortURLID] = req.body.longURL
   res.render("urls_index", templateVars)
- // res.redirect(`http://localhost:8080/urls/${newID}`)
+});
 
-  })
-
+//Login page
 app.get("/login", (req, res) => {
-
   res.render("urls_login")
+});
 
-})
-
+//upon visting /u/shortURL code - anyone can access short links.  Do not need to be logged in.
 app.get("/u/:shortURL", (req, res) => {
-
   let longURL = "";
 
   for (let user in urlDatabase){
@@ -109,24 +107,23 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-
+//confirmation on console that server is listening
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
+//delete page - upon clicking delete on urls, redirects to this page
 app.post("/urls/:id/delete", (req, res) => {
     //console.log("I'm communicating")
     let linkID = req.params.id
     delete urlDatabase[req.session.user_id][linkID]
   res.redirect("/urls");
-})
+});
 
+//edit page - upon clicking edit - links to this page
 app.post("/urls/:id", (req, res) => {
   let longURL = "";
-  console.log("I'm working")
   let thisUser = req.session.user_id
-  //console.log(req.params.id)
 
   for(let user in urlDatabase){
     for(let shortURL in urlDatabase[user]){
@@ -138,40 +135,35 @@ app.post("/urls/:id", (req, res) => {
     }
   }
 res.redirect("/urls")
-})
+});
 
 
 app.post("/login", (req,res) =>{
 
-let match = 0;
-
+  let match = 0;
   for(let currUser in users){
-    //original: if ((req.body.email === users[currUser].email) && ((req.body.password) === users[currUser].password)){
     if ((req.body.email === users[currUser].email) && ((bcrypt.compareSync(req.body.password, users[currUser].password)))) {
-
-      //res.cookie("user_id", users[currUser].id)
       req.session.user_id = users[currUser].id;
       res.redirect("/urls");
       match += 1; //adds one if a match is found
     }
   }
-//if not match is found will equal 0 and this status will show up
+//if no match is found will equal 0 and this status will show up
   if(match === 0) {
-    res.sendStatus(403)
+    res.sendStatus(403).send("User does not exist")
   }
-})
+});
 
 app.post("/logout", (req,res) => {
 
-  //res.clearCookie("user_id")
   req.session = null
   res.redirect("/urls");
 
-})
+});
 
 app.get("/register", (req, res) => {
   res.render("register");
-})
+});
 
 app.post("/register", (req, res) => {
 
@@ -197,7 +189,6 @@ app.post("/register", (req, res) => {
   newAcc.id = myID
   newAcc.email = req.body.email
 
-  //passwordsssssss --- secure ME!
   const password = req.body.password
   const hashedPassword = bcrypt.hashSync(password, 10);
   newAcc.password = hashedPassword;
@@ -205,27 +196,14 @@ app.post("/register", (req, res) => {
   users[myID] = newAcc;
   urlDatabase[myID] = newURL;
 
-  console.log(newAcc.password)
-
-  //console.log(users)
-
-  //res.cookie("user_id", myID)
   req.session.user_id = myID
   res.redirect("/urls")
 
-  console.log(users)
-
-
-})
-
-//I am testing out github branching and merging
-//playing
-//around
-//here
-//with github
+});
 
 const myshortURL = "";
 
+//Random string generator
 function generateRandomString(){
   let alpha = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
   let alphaSplit = alpha.join("").split("");
